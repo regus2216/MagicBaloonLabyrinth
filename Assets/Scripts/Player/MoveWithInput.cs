ba1e3f5ee23787e3da68208fb6a49a13239436e8
Es <radwimps.september@gmail.com>
2015-04-25 01:42:58 +0900

    ジャンプ処理追加

--- Assets/Scripts/Player/MoveWithInput.cs
+++ Assets/Scripts/Player/MoveWithInput.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 /// <summary>
 /// 入力に応じて移動する
 /// </summary>
+[RequireComponent(typeof(Rigidbody))]
 public class MoveWithInput : MonoBehaviour
 {
   [SerializeField]
@@ -15,16 +16,41 @@ public class MoveWithInput : MonoBehaviour
   private bool allowZAxis = true;
   [SerializeField, Tooltip("入力キーに応じてスプライトを反転させるのに使用")]
   private GameObject playerSprite;
+  [SerializeField, Range(0, 100)]
+  private float jumpSpeed = 10f;
+  [SerializeField, Range(0, 2), Tooltip("上昇時と下降時に速度を一定とする時間(頂点期は含まれない)")]
+  private float jumpTimeUpDown = 0.2f;
+  [SerializeField, Range(0, 10), Tooltip("ジャンプ時に頂点で序々落としていく速度の大きさ")]
+  private float jumpTopSlowly = 1f;
+
+  private bool isJumpping = false;
+
+  private Rigidbody rig_cache;
+  private Rigidbody Rigidbody
+  {
+    get
+    {
+      if(rig_cache == null)
+        rig_cache = GetComponent<Rigidbody>();
+      return rig_cache;
+    }
+  }
 
   public void Update()
   {
     //移動
     if(allowZAxis)
-      transform.Translate(moveSpeed * Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime);
-    transform.Translate(moveSpeed * Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime);
+      transform.Translate(moveSpeed * Vector3.forward * Input.GetAxisRaw("Vertical") * Time.deltaTime);
+    transform.Translate(moveSpeed * Vector3.right * Input.GetAxisRaw("Horizontal") * Time.deltaTime);
 
     //反転処理
     InverseSprite();
+
+    //ジャンプ処理
+    if(Input.GetButtonDown("Jump"))
+    {
+      StartCoroutine_Auto(Jump());
+    }
 
     //回転
     if(Input.GetButtonDown("LeftRotate"))
@@ -44,5 +70,39 @@ public class MoveWithInput : MonoBehaviour
     if(scale.x < 0 && Input.GetAxis("Horizontal") < 0)
       scale.x = 1;
     transform.localScale = scale;
+  }
+
+  private IEnumerator Jump()
+  {
+    if(isJumpping)
+      yield break;
+
+    isJumpping = true;
+
+    Rigidbody.useGravity = false;
+
+    //上がる処理
+    Rigidbody.velocity = new Vector2(0, jumpSpeed);
+
+    //しばらく一定速度で上昇
+    yield return new WaitForSeconds(jumpTimeUpDown);
+
+    //速度を下げる処理
+    while(Rigidbody.velocity.y > -jumpSpeed)
+    {
+      Rigidbody.velocity -= Vector3.up * jumpTopSlowly;
+      yield return new WaitForEndOfFrame();
+    }
+
+    //しばらく一定速度で下降
+    yield return new WaitForSeconds(jumpTimeUpDown);
+
+    Rigidbody.useGravity = true;
+    isJumpping = false;
+  }
+
+  public void OnGUI()
+  {
+    GUILayout.Label(Rigidbody.velocity.ToString());
   }
 }
