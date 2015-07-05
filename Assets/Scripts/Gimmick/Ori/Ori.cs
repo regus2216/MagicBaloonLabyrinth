bf12b54f2f780ca7df2a17711765b343584f00dd
Es_Program <radwimps.september@gmail.com>
2015-07-05 19:00:12 +0900

    add switch event and gimmick script.

---
+++ Assets/Scripts/Gimmick/Ori/Ori.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Ori : MonoBehaviour
+{
+  public SwitchEventCaller eventCaller;
+  Animator anim;
+
+  public void Awake()
+  {
+    anim = GetComponent<Animator>();
+  }
+
+
+
+
+  public void Start()
+  {
+    eventCaller.switchOnAction += () =>
+    {
+      anim.SetTrigger("Open");
+    };
+
+    eventCaller.switchOffAction += () =>
+    {
+      anim.SetTrigger("Close");
+    };
+  }
+
+
+}
--- Assets/Scripts/Gimmick/Switch/SwitchEventCaller.cs
+++ Assets/Scripts/Gimmick/Switch/SwitchEventCaller.cs
@@ -3,17 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityStandardAssets.Cameras;
 
 public class SwitchEventCaller : MonoBehaviour
 {
 
-  public event Action switchAction;
+  public event Action switchOnAction;
+  public event Action switchOffAction;
+  [SerializeField,Tooltip("イベントによってカメラの位置を変更する場合、カメラを設定するポジション")]
+  Transform camPos = null;
+  [SerializeField]
+  AutoCam cam = null;
+  Transform defaultTarget;
 
-  public void DoAction()
+  public void Start()
   {
-    if(switchAction != null)
-      switchAction();
+    defaultTarget = cam.Target;
   }
 
+
+
+
+  public void OnAction()
+  {
+    ChangeCameraTarget(camPos);
+    if(switchOnAction != null)
+      switchOnAction();
+    Invoke("SetCameraTargetDefault", 3.5f);
+  }
+
+  public void OffAction()
+  {
+    if(switchOffAction != null)
+      switchOffAction();
+  }
+
+  void ChangeCameraTarget(Transform target)
+  {
+    if(target!=null)
+      cam.SetTarget(target);
+  }
+
+  void SetCameraTargetDefault()
+  {
+    ChangeCameraTarget(defaultTarget);
+  }
 
 }
